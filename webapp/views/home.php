b2d7e53484245b753c1ba8c55327a91a857d85e6
hbsion <hbsion@qq.com>
2019-07-23 18:38:51 +0800

    DEBUYG

--- webapp/views/home.php
+++
@@ -1,4 +0,0 @@
-<?php extract(load::c('home_c')->getOutput());?>
-<?php load::v('template/header');?>
-<h1><?php echo $hello;?> </h1>
-<?php load::v('template/footer');?>
--- webapp/views/template/footer.php
+++
@@ -1,5 +0,0 @@
-<!-- footer -->
-
-<!-- javascript-->
-</body>
-</html>
